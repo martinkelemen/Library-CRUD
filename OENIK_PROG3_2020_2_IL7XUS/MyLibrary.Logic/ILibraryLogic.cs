22a87ea6803322ffedba6f9b974de72b579ba8d0
martin_kelemen <novaaahh@gmail.com>
2020-10-28 21:06:19 +0100

    Add interface specifications

--- OENIK_PROG3_2020_2_IL7XUS/MyLibrary.Logic/ILibraryLogic.cs
+++ OENIK_PROG3_2020_2_IL7XUS/MyLibrary.Logic/ILibraryLogic.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Castle.DynamicProxy.Contributors;
+using MyLibrary.Data;
+using MyLibrary.Repository;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,30 @@ namespace MyLibrary.Logic
 {
     interface ILibraryLogic
     {
+        Book GetBookById(int id);
+
+        BookRental GetBookRentalById(int id);
+
+        IList<Book> GetAllBooks();
+
+        IList<BookRental> GetAllBookRentals();
+
+        void AddBook(Book bookInstance);
+
+        void AddRental(BookRental rentalInstance);
+
+        void ChangeBookLanguage(string id, string newLanguage);
+
+        void ChangeBookPublisher(string id, string newPublisher);
+
+        void ChangeBookYear(string id, int newYear);
+
+        void ChangeBookRentalDays(int id, int newDays);
+
+        IList<string> GetRentByLanguage();
+
+        IList<string> GetRentByMembership();
+
+        IList<string> ListAllRents();
     }
 }
--- OENIK_PROG3_2020_2_IL7XUS/MyLibrary.Logic/IPersonLogic.cs
+++ OENIK_PROG3_2020_2_IL7XUS/MyLibrary.Logic/IPersonLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyLibrary.Data;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,32 @@ namespace MyLibrary.Logic
 {
     interface IPersonLogic
     {
+        Renter GetRenterById(int id);
+
+        Worker GetWorkerById(int id);
+
+        IList<Renter> GetAllRenters();
+
+        IList<Worker> GetAllWorkers();
+
+        void AddRenter(Renter renterInstance);
+
+        void AddWorker(Worker workerInstance);
+
+        void ChangeRenterAddress(int id, string newAddress);
+
+        void ChangeRenterEmail(int id, string newEmail);
+
+        void ChangeRenterMembershipType(int id, string newMembershipType);
+
+        void ChangeRenterName(int id, string newName);
+
+        void ChangeWorkerAddress(int id, string newAddress);
+
+        void ChangeWorkerEmail(int id, string newEmail);
+
+        void ChangeWorkerName(int id, string newName);
+
+        void ChangeWorkerSalary(int id, int newSalary);
     }
 }
