cec0d25504fa355c6f4139772acdfebb4e8bb739
martin_kelemen <novaaahh@gmail.com>
2020-10-28 21:08:15 +0100

    Implement interfaces, list methods.

--- OENIK_PROG3_2020_2_IL7XUS/MyLibrary.Logic/LibraryLogic.cs
+++ OENIK_PROG3_2020_2_IL7XUS/MyLibrary.Logic/LibraryLogic.cs
@@ -1,10 +1,86 @@
-﻿using System;
+﻿using MyLibrary.Data;
+using MyLibrary.Repository;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MyLibrary.Logic
 {
-    class LibraryLogic
+    class LibraryLogic : ILibraryLogic
     {
+        private IBookRepository bookRepository;
+        private IBookRentalRepository bookRentalRepository;
+
+        public LibraryLogic(IBookRepository bookRepository, IBookRentalRepository bookRentalRepository)
+        {
+            this.bookRepository = bookRepository;
+            this.bookRentalRepository = bookRentalRepository;
+        }
+
+        public void AddBook(Book bookInstance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRental(BookRental rentalInstance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeBookLanguage(string id, string newLanguage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeBookPublisher(string id, string newPublisher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeBookRentalDays(int id, int newDays)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeBookYear(string id, int newYear)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<BookRental> GetAllBookRentals()
+        {
+            return this.bookRentalRepository.GetAll().ToList();
+        }
+
+        public IList<Book> GetAllBooks()
+        {
+            return this.bookRepository.GetAll().ToList();
+        }
+
+        public Book GetBookById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookRental GetBookRentalById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<string> GetRentByLanguage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<string> GetRentByMembership()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<string> ListAllRents()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- OENIK_PROG3_2020_2_IL7XUS/MyLibrary.Logic/PersonLogic.cs
+++ OENIK_PROG3_2020_2_IL7XUS/MyLibrary.Logic/PersonLogic.cs
@@ -1,10 +1,92 @@
-﻿using System;
+﻿using MyLibrary.Data;
+using MyLibrary.Repository;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MyLibrary.Logic
 {
-    class PersonLogic
+    class PersonLogic : IPersonLogic
     {
+        private IRenterRepository renterRepository;
+
+        private IWorkerRepository workerRepository;
+
+        public PersonLogic(IRenterRepository renterRepository, IWorkerRepository workerRepository)
+        {
+            this.renterRepository = renterRepository;
+            this.workerRepository = workerRepository;
+        }
+
+        public void AddRenter(Renter renterInstance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddWorker(Worker workerInstance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeRenterAddress(int id, string newAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeRenterEmail(int id, string newEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeRenterMembershipType(int id, string newMembershipType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeRenterName(int id, string newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeWorkerAddress(int id, string newAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeWorkerEmail(int id, string newEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeWorkerName(int id, string newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeWorkerSalary(int id, int newSalary)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Renter> GetAllRenters()
+        {
+            return this.renterRepository.GetAll().ToList();
+        }
+
+        public IList<Worker> GetAllWorkers()
+        {
+            return this.workerRepository.GetAll().ToList();
+        }
+
+        public Renter GetRenterById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Worker GetWorkerById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
