c0b2a47b0728ae1fc29d7e26bb96c21f45024da9
martin_kelemen <novaaahh@gmail.com>
2020-10-29 16:05:17 +0100

    Create Factory class

---
+++ OENIK_PROG3_2020_2_IL7XUS/MyLibrary.Program/Factory.cs
@@ -0,0 +1,37 @@
+﻿namespace MyLibrary.Program
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using MyLibrary.Data;
+    using MyLibrary.Logic;
+    using MyLibrary.Repository;
+
+    public class Factory
+    {
+        public Factory()
+        {
+            this.LibraryContext = new LibraryContext();
+            this.BookRepository = new BookRepository(this.LibraryContext);
+            this.RenterRepository = new RenterRepository(this.LibraryContext);
+            this.WorkerRepository = new WorkerRepository(this.LibraryContext);
+            this.BookRentalRepository = new BookRentalRepository(this.LibraryContext);
+            this.LibraryLogic = new LibraryLogic(this.BookRepository, this.BookRentalRepository);
+            this.PersonLogic = new PersonLogic(this.RenterRepository, this.WorkerRepository);
+        }
+
+        public LibraryContext LibraryContext { get; }
+
+        public BookRepository BookRepository { get; }
+
+        public RenterRepository RenterRepository { get; }
+
+        public WorkerRepository WorkerRepository { get; }
+
+        public BookRentalRepository BookRentalRepository { get; }
+
+        public LibraryLogic LibraryLogic { get; }
+
+        public PersonLogic PersonLogic { get; }
+    }
+}
