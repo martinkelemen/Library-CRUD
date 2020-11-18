7b2e869bb82cef4fdfc48547e54622675ef34615
martin_kelemen <novaaahh@gmail.com>
2020-11-18 13:52:31 +0100

    Create classes for the Logic tests

---
+++ OENIK_PROG3_2020_2_IL7XUS/MyLibrary.Logic.Tests/LibraryLogicTests.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="LibraryLogicTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MyLibrary.Logic.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class LibraryLogicTests
+    {
+    }
+}
